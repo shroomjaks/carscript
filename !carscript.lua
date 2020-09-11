b0fc380ff46f637196267401c4b4d68b88baf201
Lozarth <46830521+MonkoTubeYT@users.noreply.github.com>
2020-09-10 23:13:47 -0400

    Update !carscript.lua

--- !carscript.lua
+++ !carscript.lua
@@ -1,9 +1,1 @@
-game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 70
-AudioID = "1389847916"
-AnimationId = "129342287"
-local Anim = Instance.new("Animation")
-Anim.AnimationId = "rbxassetid://"..AnimationId
-local k = game.Players.LocalPlayer.Character.Humanoid:LoadAnimation(Anim)
-k:Play()
-k:AdjustSpeed(1)
-setclipboard(AudioID)
+game.Players.LocalPlayer.Character.Humanoid.WalkSpeed=70 AudioID="1389847916" AnimationId="129342287" local _=Instance.new("Animation") _.AnimationId="rbxassetid://"..AnimationId local _=game.Players.LocalPlayer.Character.Humanoid:LoadAnimation(_) _:Play() _:AdjustSpeed(1) setclipboard(AudioID)
