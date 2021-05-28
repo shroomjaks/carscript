bb9739eae4cfa5b1b139b18c8e549f04a8ffa1a4
Lozarth <46830521+MonkoTubeYT@users.noreply.github.com>
2021-05-27 20:43:28 -0400

    Update !carscript.lua

--- !carscript.lua
+++ !carscript.lua
@@ -1,4 +1,4 @@
---testing2
+--trololo
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/IreXion-UI-Library/main/IreXion%20UI%20Library"))()
 
 local Gui = Library:AddGui({
